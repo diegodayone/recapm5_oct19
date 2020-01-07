737c744fb5b7828b223f423b856513ca856930f9
Diego Banovaz <diego@strive.school>
2020-01-07 11:16:53 +0100

    second break

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express") //load express from the package
 const bookRouter = require("./src/services/books") //automatically goes to index.js
+const path = require("path")
+const cors = require("cors")
 
 const server = express();
+
+server.use(cors())
+server.use(express.json())
+
+//app.use('/static', express.static(path.join(__dirname, 'public')))
+server.use("/images", express.static(path.join(__dirname, "images")))
 
 server.get("/", (req, res) => {
     res.send("I'm listening.")
--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const fs = require("fs-extra")
 const path = require("path")
+const { check, validationResult, sanitizeBody } = require('express-validator');
+const multer = require("multer")
+const multerConfig = multer()
+
 
 const fileLocation = path.join(__dirname, "books.json")
 
@@ -45,6 +49,54 @@ bookRouter.get("/:id", async (req, res)=>{
         res.send(book)
     else
         res.status(404).send("NOT FOUND")
+})
+
+bookRouter.post("/",
+    [   check("asin").isLength({min: 10, max:10}).withMessage("Hey man, the ASIN should have 10 chars"),
+        check("name").isString({ min: 3}).withMessage("The name should have at least 3 chars"), 
+        check("price").isNumeric().withMessage("Only numeric values are accepted"),
+        check("category").isString(),
+        sanitizeBody("price").toFloat()],
+     async(req,res)=>{
+    //req.body = undefined? you should use server.use(express.json()) in your server file
+    //req.body = {}? check the content type of your request (should be application/json)    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    const books = await readBooks();
+    books.push(req.body)
+    // usually you need to generate fields as:
+    // id
+    // creation date
+    // update... etc
+    // user that created the entry
+    await fs.writeFile(fileLocation, JSON.stringify(books))
+    res.send(req.body)
+})
+
+bookRouter.post("/:asin", multerConfig.single("bookCover"), async (req, res)=>{
+    //But... do we have a book with that ASIN?
+    const books = await readBooks(); //get the books
+    const book = books.find(b => b.asin === req.params.asin) //find the book by ASIN
+    if (!book) //if no book matches ==> not found
+        return res.status(404).send("NOT FOUND")
+
+    //else...
+    const fileName = req.params.asin + path.extname(req.file.originalname) //create a new filename like ASIN.ext
+    const newImageLocation = path.join(__dirname, "../../../images", fileName); //create the path to my images folder
+    await fs.writeFile(newImageLocation, req.file.buffer) //write down the image on the folder
+
+    book.img = req.protocol + '://' + req.get('host') + "/images/" + fileName; //update the book object
+    //book.img = "http://localhost:4000/images/1231231230.jpg; 
+    await fs.writeFile(fileLocation, JSON.stringify(books)) //write down the whole books array
+
+    // How can we save the picture url now???
+    res.send({
+        result: "OK",
+        url: book.img
+    })
 })
 
 
